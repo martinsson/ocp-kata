edb2d750731dcee8ca8b1ba88eea965badac6fe4
Johan MARTINSSON <martinsson.johan@gmail.com>
2017-05-10 02:11:57 +0200

    impl crossdiscount

---
+++ src/main/java/it/xpug/ocp/checkout/CrossDiscount.java
@@ -0,0 +1,26 @@
+package it.xpug.ocp.checkout;
+
+import java.util.List;
+
+public class CrossDiscount {
+    private final String discountee;
+    private final String discounter;
+    private final int minQuantity;
+    private final int amount;
+
+    public CrossDiscount(String discountee, String discounter, int minQuantity, int amount) {
+
+        this.discountee = discountee;
+        this.discounter = discounter;
+        this.minQuantity = minQuantity;
+        this.amount = amount;
+    }
+
+    public int discount(List<String> shoppingBasket) {
+        long numberOfDiscounters = shoppingBasket.stream().filter(item -> discounter.equals(item)).count();
+        if (shoppingBasket.contains(discountee) && numberOfDiscounters >= minQuantity) {
+            return amount;
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/it/xpug/ocp/checkout/CrossDiscountTest.java
@@ -0,0 +1,26 @@
+package it.xpug.ocp.checkout;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CrossDiscountTest {
+    @Test
+    public void discountDoesntApplyIfCombination() throws Exception {
+
+        CrossDiscount crossDiscount = new CrossDiscount("E", "C", 2, 26);
+        List<String> shoppingBasket = Arrays.asList("E", "C");
+        Assert.assertEquals(0, crossDiscount.discount(shoppingBasket));
+    }
+
+    @Test
+    public void discountAppliesIfCombination() throws Exception {
+
+        CrossDiscount crossDiscount = new CrossDiscount("E", "C", 2, 26);
+
+        List<String> shoppingBasket = Arrays.asList("E", "C", "C");
+        Assert.assertEquals(26, crossDiscount.discount(shoppingBasket));
+    }
+}
